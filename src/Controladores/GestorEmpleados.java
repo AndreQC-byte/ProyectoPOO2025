15cb86e3bedccf0d835bdb46c6ca8b8c476d1ff5
Carlos Castañeda <O26304@O26304.lab.aca>
2025-11-21 08:44:18 -0500

    Añadí la clase controladora gestor de empleados

---
+++ src/Controladores/GestorEmpleados.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controladores;
+
+import HOSPITAL.Tipos_de_empleados.cEmpleado;
+
+/**
+ *
+ * @author O26304
+ */
+public class GestorEmpleados {
+
+}

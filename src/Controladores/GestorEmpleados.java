758d0991acd3b06d981fb343c1d368d11d7b708c
Carlos Castañeda <>
2025-11-21 08:56:32 -0500

    Modifique el controlador Gestor de Empleados

--- src/Controladores/GestorEmpleados.java
+++ src/Controladores/GestorEmpleados.java
@@ -11,5 +11,17 @@ import HOSPITAL.Tipos_de_empleados.cEmpleado;
  * @author O26304
  */
 public class GestorEmpleados {
-
+    private cEmpleado [] listaEmpleados;
+    private int nroEmpleados;
+    
+    public GestorEmpleados() {
+        this.listaEmpleados = new cEmpleado[10];
+        this.nroEmpleados = 0;
+    }
+    
+    public void AgregarEmpleado(String pUsername, String pPassword, String pRole){
+        this.listaEmpleados[this.nroEmpleados] = new cEmpleado(pUsername, pPassword, pRole);
+    }
+    
+    
 }

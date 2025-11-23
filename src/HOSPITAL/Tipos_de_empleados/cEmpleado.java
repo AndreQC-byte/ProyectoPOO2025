a942236e0798e2eb36f2d84fd765cd151d24eb6d
Carlos Castañeda <wedee@DESKTOP-C3TRNHV>
2025-11-22 19:10:49 -0500

    Agregue getters y setters a la clase Empleado

--- src/HOSPITAL/Tipos_de_empleados/cEmpleado.java
+++ src/HOSPITAL/Tipos_de_empleados/cEmpleado.java
@@ -26,6 +26,54 @@ public abstract class  cEmpleado {
         this.datosContacto = datosContacto;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDatosContacto() {
+        return datosContacto;
+    }
+
+    public void setDatosContacto(String datosContacto) {
+        this.datosContacto = datosContacto;
+    }
+
    
     
 }

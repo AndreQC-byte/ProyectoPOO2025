8453007643906deb4731df8c08504bb70b528d41
Hp <Hp@192.168.1.37>
2025-11-29 15:19:36 -0500

    consultoria

--- src/Controladores/GestorConsultorios.java
+++ src/Controladores/GestorConsultorios.java
@@ -4,86 +4,58 @@
  */
 package Controladores;
 
-import HOSPITAL.Atenciones.cConsultorios.cConsultorios;
+import CREACIONES.cConsultorios;
 
-/**
- *
- * @author O26302
- */
 public class GestorConsultorios {
-    private cConsultorios [] listaConsultorios;
+
+    private cConsultorios[] listaConsultorios;
     private int nroConsultorios;
-    
+
     public GestorConsultorios() {
-        this.listaConsultorios = new cConsultorios[10];
-        this.nroConsultorios = 0;
+        listaConsultorios = new cConsultorios[50];
+        nroConsultorios = 0;
     }
-    
-    public void AgregarConsultorio(cConsultorios empleado){
-        this.listaConsultorios[this.nroConsultorios] = empleado;
-        this.nroConsultorios++;
+
+    public void agregarConsultorio(cConsultorios c) {
+        listaConsultorios[nroConsultorios] = c;
+        nroConsultorios++;
     }
-    
-    public void Eliminar(String codigo)
-    {
-        for(int i=0;i<nroConsultorios;i++)
-        {
-            if(listaConsultorios[i].getCodigo().equalsIgnoreCase(codigo))
-            {
-                for(int j=i;j<nroConsultorios-1;j++)
-                {
-                    listaConsultorios[j]=listaConsultorios[j+1];
+
+    public void eliminar(String codigo) {
+        for (int i = 0; i < nroConsultorios; i++) {
+            if (listaConsultorios[i].getCodigo().equalsIgnoreCase(codigo)) {
+
+                for (int j = i; j < nroConsultorios - 1; j++) {
+                    listaConsultorios[j] = listaConsultorios[j + 1];
                 }
-                listaConsultorios[nroConsultorios-1]=null;
+
+                listaConsultorios[nroConsultorios - 1] = null;
                 nroConsultorios--;
+                return;
             }
         }
     }
-    
-   public void modificar(String codigo, cConsultorios nueva_data){
-        for (int i = 0; i < this.nroConsultorios; i++) {
-            if (this.listaConsultorios[i].getCodigo().equals(codigo)){
-                this.listaConsultorios[i].setEstado(nueva_data.getEstado());
-                this.listaConsultorios[i].setHorario(nueva_data.getHorario());
-                this.listaConsultorios[i].setCodigo(nueva_data.getCodigo());
 
+    public void modificar(String codigo, cConsultorios nuevo) {
+        for (int i = 0; i < nroConsultorios; i++) {
+            if (listaConsultorios[i].getCodigo().equalsIgnoreCase(codigo)) {
+
+                listaConsultorios[i].setEspecialidad(nuevo.getEspecialidad());
+                listaConsultorios[i].setEstado(nuevo.getEstado());
+                listaConsultorios[i].setDia(nuevo.getDia());
+                listaConsultorios[i].setAmbiente(nuevo.getAmbiente());
+                listaConsultorios[i].setCodigo(nuevo.getCodigo());
+
+                return;
             }
-            
-        }               
+        }
     }
-   
-   public cConsultorios getConsultorio(String codigo)
-   {
-       return this.listaConsultorios[0];
-   }
-   
-   public boolean ValidarUsuario(String login,String password)
-   {
-       return true;
-   }
 
     public cConsultorios[] getListaConsultorios() {
         return listaConsultorios;
     }
 
-    public void setListaConsultorios(cConsultorios[] listaConsultorios) {
-        this.listaConsultorios = listaConsultorios;
-    }
-
     public int getNroConsultorios() {
         return nroConsultorios;
     }
-
-    public void setNroConsultorios(int nroConsultorios) {
-        this.nroConsultorios = nroConsultorios;
-    }
-            
-            
-            
-            
-            
-
-    
-    
-    
 }

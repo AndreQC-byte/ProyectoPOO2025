45fa9a114316632660fba3f891e6f0370fa24c00
Carlos Castañeda <wedee@DESKTOP-C3TRNHV>
2025-11-28 23:52:49 -0500

    Gabriel agrego todo lo que faltaba en clase consultorios

--- src/HOSPITAL/Atenciones/cConsultorios/cConsultorios.java
+++ src/HOSPITAL/Atenciones/cConsultorios/cConsultorios.java
@@ -9,5 +9,39 @@ package HOSPITAL.Atenciones.cConsultorios;
  * @author wedee
  */
 public class cConsultorios {
+    private String codigo;
+    private String estado;
+    private String horario;
+
+    public cConsultorios(String codigo, String estado, String horario) {
+        this.codigo = codigo;
+        this.estado = estado;
+        this.horario = horario;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
     
 }

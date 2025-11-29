640f89076bae63a251013626626c3f0920ec2177
User <User@192.168.1.35>
2025-11-29 13:06:34 -0500

    chi

--- src/Interfaces_GUI/Sistema.java
+++ src/Interfaces_GUI/Sistema.java
@@ -6,6 +6,7 @@ package Interfaces_GUI;
 import HOSPITAL.Tipos_de_empleados.Administrador;
 import HOSPITAL.Tipos_de_empleados.cCajero;
 import HOSPITAL.Tipos_de_empleados.cMedico;
+import HOSPITAL.Tipos_de_empleados.cRecepcionista;
 
 /**
  *
@@ -17,7 +18,7 @@ public class Sistema {
         DataAdmin.empleados.AgregarEmpleado(new Administrador("12121212", "Adriana", "Cabero", "948987987", "Admin", "acabero", "123456"));
         DataAdmin.empleados.AgregarEmpleado(new cMedico("12345678", "Carlos", "Garcia", "948987987", "cgarcia", "123456", "Pediatra"));
         DataAdmin.empleados.AgregarEmpleado(new cCajero("923821", "Mel Gibson", "Veracruz", "093219831", "cajero", "soytupapi", "123456"));
-        
+        DataAdmin.empleados.AgregarEmpleado(new cRecepcionista("932842", "Jasson", "Mackgregor", "92383", "recepcionista", "", "123456"));
         Login x = new Login();
         x.setVisible(true);
     }
